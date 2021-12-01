3f73cecde0ff6cb3cf157bea636a0944682e7ea4
Quetzy Garcia <quetzyg@altek.org>
2021-12-01 17:43:48 +0000

    feat(day 1): add initial implementation

---
+++ cmd/day01/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/quetzyg/aoc2021/pkg/input"
+)
+
+const windowSize = 3
+
+var measurements []int
+
+var parseFunc = func(s string) error {
+	depth, err := strconv.Atoi(s)
+
+	if err != nil {
+		return err
+	}
+
+	measurements = append(measurements, depth)
+
+	return nil
+}
+
+func part1(data []int) int {
+	var (
+		prev  int
+		total int
+	)
+
+	for _, depth := range data {
+		if prev == 0 {
+			prev = depth
+
+			continue
+		}
+
+		if depth > prev {
+			total++
+		}
+
+		prev = depth
+	}
+
+	return total
+}
+
+func part2(data []int, window int) int {
+	var (
+		prev  int
+		total int
+	)
+
+	for i := 0; i <= len(data)-window; i++ {
+		sum := 0
+
+		for _, depth := range data[i : i+window] {
+			sum += depth
+		}
+
+		if prev == 0 {
+			prev = sum
+
+			continue
+		}
+
+		if sum > prev {
+			total++
+		}
+
+		prev = sum
+	}
+
+	return total
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("missing input path argument")
+	}
+
+	f, err := os.Open(os.Args[1])
+
+	if err != nil {
+		log.Fatalf("unable to open input: %v", err)
+	}
+
+	defer f.Close()
+
+	err = input.Parse(f, bufio.ScanLines, parseFunc)
+
+	if err != nil {
+		log.Fatalf("unable to parse input: %v", err)
+	}
+
+	fmt.Printf("part #1: %d\n", part1(measurements))
+	fmt.Printf("part #2: %d\n", part2(measurements, windowSize))
+}
