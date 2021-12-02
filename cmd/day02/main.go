7a42cba99620506566644a50fce45586101ab772
Quetzy Garcia <quetzyg@altek.org>
2021-12-02 14:50:18 +0000

    feat(day 2): add initial implementation + sample input

---
+++ cmd/day02/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/quetzyg/aoc2021/pkg/input"
+)
+
+const (
+	forward = "forward"
+	down    = "down"
+	up      = "up"
+)
+
+var (
+	horizontal int
+	depth1     int
+	depth2     int
+	aim        int
+)
+
+var parseFunc = func(s string) error {
+	fields := strings.Fields(s)
+
+	if len(fields) != 2 {
+		return fmt.Errorf("expected 2 fields per line, got %d", len(fields))
+	}
+
+	units, err := strconv.Atoi(fields[1])
+
+	if err != nil {
+		return err
+	}
+
+	switch fields[0] {
+	case up:
+		depth1 -= units
+		aim -= units
+	case down:
+		depth1 += units
+		aim += units
+	case forward:
+		horizontal += units
+		depth2 += aim * units
+	default:
+		return fmt.Errorf("unknown command: %s", fields[0])
+	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("missing input path argument")
+	}
+
+	f, err := os.Open(os.Args[1])
+
+	if err != nil {
+		log.Fatalf("unable to open input: %v", err)
+	}
+
+	defer f.Close()
+
+	err = input.Parse(f, bufio.ScanLines, parseFunc)
+
+	if err != nil {
+		log.Fatalf("unable to parse input: %v", err)
+	}
+
+	fmt.Printf("part #1: %d\n", horizontal*depth1)
+	fmt.Printf("part #2: %d\n", horizontal*depth2)
+}
